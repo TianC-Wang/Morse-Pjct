5ada2cdce835d1c41c8b149d379ec467db0f5eb3
= <=>
2020-07-12 12:23:44 +0800

    Bug fix

--- ports.h
+++ ports.h
@@ -1,85 +1,88 @@
-class Port
+namespace Ports
 {
-protected:
-    int port;
-    Port(int port)
+    class Port
     {
-        this->port = port;
-    }
+    protected:
+        int port;
+        Port(int port)
+        {
+            this->port = port;
+        }
 
-public:
-    void setPort(int port)
+    public:
+        void setPort(int port)
+        {
+            this->port = port;
+        }
+    };
+    class ADPort : public Port
     {
-        this->port = port;
-    }
-};
-class ADPort : public Port
-{
-protected:
-    bool reversed;
-    ADPort(int port, bool reversed) : Port(port) { this->reversed = reversed; }
+    protected:
+        bool reversed;
+        ADPort(int port, bool reversed) : Port(port) { this->reversed = reversed; }
 
-public:
-    void setReverse(bool reversed)
+    public:
+        void setReverse(bool reversed)
+        {
+            this->reversed = reversed;
+        }
+    };
+    class DIPort : public ADPort
     {
-        this->reversed = reversed;
-    }
-};
-class DIPort : public ADPort
-{
-public:
-    DIPort(int port, bool reversed = false) : ADPort(port, reversed) { pinMode(port, INPUT); }
-    bool get(void)
+    public:
+        DIPort(int port, bool reversed = false) : ADPort(port, reversed) { pinMode(port, INPUT); }
+        bool get(void)
+        {
+            return digitalRead(port) ^ reversed;
+        }
+        void setPort(int port)
+        {
+            this->port = port;
+            pinMode(port, INPUT);
+        }
+    };
+    class DOPort : public ADPort
     {
-        return digitalRead(port) ^ reversed;
-    }
-    void setPort(int port)
+    public:
+        DOPort(int port, bool reversed = false) : ADPort(port, reversed) { pinMode(port, OUTPUT); }
+        void set(bool HoL)
+        {
+            digitalWrite(port, HoL ^ reversed);
+        }
+        void setPort(int port)
+        {
+            this->port = port;
+            pinMode(port, OUTPUT);
+        }
+    };
+    class AIPort : public ADPort
     {
-        this->port = port;
-        pinMode(port, INPUT);
-    }
-};
-class DOPort : public ADPort
-{
-public:
-    DOPort(int port, bool reversed = false) : ADPort(port, reversed) { pinMode(port, OUTPUT); }
-    void set(bool HoL)
+    public:
+        AIPort(int port, bool reversed = false) : ADPort(port, reversed) { pinMode(port, INPUT); }
+        int get(void)
+        {
+            int temp;
+            temp = analogRead(port);
+            return reversed ? 255 - temp : temp;
+        }
+        void setPort(int port)
+        {
+            this->port = port;
+            pinMode(port, INPUT);
+        }
+    };
+    class AOPort : public ADPort
     {
-        digitalWrite(port, HoL ^ reversed);
-    }
-    void setPort(int port)
-    {
-        this->port = port;
-        pinMode(port, OUTPUT);
-    }
-};
-class AIPort : public ADPort
-{
-public:
-    AIPort(int port, bool reversed = false) : ADPort(port, reversed) { pinMode(port, INPUT); }
-    bool get(void)
-    {
-        int temp;
-        temp = analogRead(port);
-        return reversed ? 255 - temp : temp;
-    }
-    void setPort(int port)
-    {
-        this->port = port;
-        pinMode(port, INPUT);
-    }
-};
-class AOPort : public ADPort
-{
-public:
-    AOPort(int port, bool reversed = false) : ADPort(port, reversed) { pinMode(port, OUTPUT); }
-    void set(int Lvl)
-    {
-        analogWrite(port, reversed ? 255 - Lvl : Lvl);
-    }
-    void setPort(int port)
-    {
-        this->port = port;
-        pinMode(port, OUTPUT);
-    }
-};
+    public:
+        AOPort(int port, bool reversed = false) : ADPort(port, reversed) { pinMode(port, OUTPUT); }
+        void set(int Lvl)
+        {
+            analogWrite(port, reversed ? 255 - Lvl : Lvl);
+        }
+        void setPort(int port)
+        {
+            this->port = port;
+            pinMode(port, OUTPUT);
+        }
+    };
+}
